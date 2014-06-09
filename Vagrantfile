84f0b813a53c4e64881304a991d9e52337e160a2
Marcus Orochena <marcus.orochena@gmail.com>
2014-06-09 15:43:01 -0400

    added node

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,21 @@
-Vagrant.configure(“2”) do |config|
-  config.vm.provision “puppet”
+## variables
+LOCAL_SYNCED_FOLDER = ""
+FORWARDED_PORT = "3000"
+
+
+Vagrant.configure("2") do |config|
+
+  config.vm.box = "ubuntu/trusty64"
+
+  #config.vm.network :forwarded_port, guest: FORWARDED_PORT, host: FORWARDED_PORT
+  #config.vm.synced_folder LOCAL_SYNCED_FOLDER, "/home/vagrant/projects"
+
+  #provision with puppet
+  config.vm.provision "puppet" do |puppet|
+  end
+
+  #provide more memory to the box
+  config.vm.provider :virtualbox do |vb|
+     vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,23 +1,15 @@
+import "puppet-apt/manifests/init.pp"
+import "puppet-apt/manifests/ppa.pp"
+
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
 
 # --- Preinstall Stage ---
 
-stage { 'preinstall':
-  before => Stage['main']
-}
-
-class apt_get_update {
-  exec {
-    "apt-get -y update":
-      path: "usr/bin"
-  }
-}
-
-class { 'apt_get_update':
-  stage => preinstall
-}
 
 # --- Packages ---
 
+# Rails Dependencies
 package { 'curl':
   ensure => installed
 }
@@ -25,3 +17,39 @@ package { 'curl':
 package { ['sqlite3', 'libsqlite3-dev']:
   ensure => installed;
 }
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+# Node Dependencies
+package { ['python', 'g++', 'make', 'software-properties-common']:
+  ensure => installed
+}
+
+# Editing Tools
+
+package { 'vim':
+  ensure => latest
+}
+
+package { 'tmux':
+  ensure => latest
+}
+
+
+# --- Node ---------------------------------------------------------------------
+
+apt::ppa { 'ppa:chris-lea/node.js': }
+
+package { 'nodejs': 
+  ensure => 'installed'
+}
+
+apt::ppa ['ppa:chris-lea/node.js'] -> Package['nodejs']
+
+
